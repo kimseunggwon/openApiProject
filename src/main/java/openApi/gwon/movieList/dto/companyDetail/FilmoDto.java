eb2864222a03e29429d0007fa6ab488023857f01
kimseunggwon <dmltcl1@naver.com>
2024-04-26 17:08:44 +0900

    영화사 상세정보 filmo 테이블 COMPANYPARTNM 필드 varchar - > text String - > JSON 배열 타입으로 수정중 todo :

--- src/main/java/openApi/gwon/movieList/dto/companyDetail/FilmoDto.java
+++ src/main/java/openApi/gwon/movieList/dto/companyDetail/FilmoDto.java
@@ -1,7 +1,12 @@
 package openApi.gwon.movieList.dto.companyDetail;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class FilmoDto {
@@ -17,5 +22,9 @@ public class FilmoDto {
     private String filmoMovieNm;
 
     @JsonProperty("companyPartNm")
-    private String filmoCompanyPartNm;
+    @JsonSerialize(using = ToStringSerializer.class) // JSON 배열을 문자열로 변환
+    @JsonDeserialize(as = List.class) // 문자열을 JSON 배열로 변환
+    private List<String> filmoCompanyPartNms;
+
+
 }
